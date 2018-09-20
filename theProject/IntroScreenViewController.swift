15a969f418b614bc61528a0a6d77a138a1b0e0e0
Chris Rodriguez <chrismail627@gmail.com>
2018-09-20 13:41:19 -0700

    Getting the log in and sign up started.

--- theProject/IntroScreenViewController.swift
+++ theProject/IntroScreenViewController.swift
@@ -10,44 +10,33 @@ import UIKit
 
 class IntroScreenViewController: UIViewController {
     
+    // MARK: Outlets
     @IBOutlet weak var logInButton: UIButton! // roundButton!
+    @IBOutlet weak var signUpButton: UIButton!
     
+    // MARK: Actions
+    @IBAction func logInButtonTapped(_ sender: Any) {
+        performSegue(withIdentifier: "toLogIn", sender: self)
+    }
+    
+    @IBAction func signInButtonTapped(_ sender: Any) {
+        performSegue(withIdentifier: "toSignUp", sender: self)
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         logInButton.applyDesign()
-        
+        signUpButton.applyDesign()
     }
-    
 }
 
 extension UIButton {
     func applyDesign() {
-        //self.backgroundColor = UIColor.darkGray
+        self.backgroundColor = UIColor.darkGray.withAlphaComponent(0.1)
         self.layer.borderWidth = 2
         self.layer.borderColor = UIColor.darkGray.cgColor
-        self.layer.cornerRadius = self.frame.height / 2 // Rounds corners completely.
-        self.setTitleColor(UIColor.white, for: .normal)
-        
-        /*
-        self.layer.shadowColor = UIColor.darkGray.cgColor
-        self.layer.shadowRadius = 4
-        self.layer.shadowOpacity = 0.5
-        self.layer.shadowOffset = CGSize(width: 0, height: 0)
-        */
-    }
-}
-
-class roundButton: UIButton { // Can be applied in the inspector in storyboard.
-    override func didMoveToWindow() {
-        self.backgroundColor = UIColor.darkGray
-        self.layer.cornerRadius = self.frame.height / 2 // Rounds corners completely.
-        self.setTitleColor(UIColor.white, for: .normal)
-        self.layer.shadowColor = UIColor.darkGray.cgColor
-        self.layer.shadowRadius = 4
-        
-        self.layer.shadowOpacity = 0.5
-        self.layer.shadowOffset = CGSize(width: 0, height: 0)
+        //self.layer.cornerRadius = self.frame.height / 2 // Rounds corners completely.
+        self.setTitleColor(UIColor.darkGray, for: .normal)
     }
 }
---
+++ theProject/SignUpViewController.swift
@@ -0,0 +1,9 @@
+//
+//  SignUpViewController.swift
+//  theProject
+//
+//  Created by Chris Rodriguez on 9/20/18.
+//  Copyright © 2018 Chris. All rights reserved.
+//
+
+import Foundation
